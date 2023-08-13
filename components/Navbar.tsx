25e4a6a7d25fdf4282d2b8bb19ae993b7d2b5ccc
Lucas <lucascomamala@gmail.com>
2023-08-12 19:24:50 -0600

    Desktop navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,19 +11,55 @@ const Navbar = () => {
         <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
           <Image className='w-14'   src={logoSvg} alt='logo' width={52} height={52} />
         </motion.div>
-        <div>
-          <ul className='flex text-[15px] gap-7'>
+        <div className='hidden mdl:inline-flex items-center gap-7'>
+          <ul className='flex text-[13px] gap-7'>
             <Link
               href='#home'
               className=' flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link'
             >
-              <li>Home</li>
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.1 }}
+              >
+                Home
+              </motion.li>
             </Link>
             <Link
-              href='#home'
+              href='#about'
               className=' flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link'
             >
-              <li><span>01.</span> About</li>
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.1, delay: 0.2 }}
+              >
+                <span className='text-textGreen'>02.</span> Experience
+              </motion.li>
+            </Link>
+            <Link
+              href='#projects'
+              className=' flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link'
+            >
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.1, delay: 0.3 }}
+              >
+                <span className='text-textGreen'>03.</span> Projects
+              </motion.li>
+            </Link>
+            <Link
+              href='#contact'
+              className=' flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link'
+            >
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.1, delay: 0.4 }}
+              >
+                <span className='text-textGreen'>03.</span> Contact
+              </motion.li>
             </Link>
           </ul>
         </div>
