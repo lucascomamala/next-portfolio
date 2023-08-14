3fd81c1afd46ac021dc241960ab909136d2d1cbf
Lucas <lucascomamala@gmail.com>
2023-08-13 23:21:08 -0600

    Archive

--- components/Archive.tsx
+++ components/Archive.tsx
@@ -1,6 +1,11 @@
+import { useState } from 'react'
+import { motion } from 'framer-motion'
+
 import { ArchiveCard } from './'
 
 const Archive = () => {
+  const [showMore, setShowMore] = useState(false)
+
   return (
     <div className='max-w-contentContainer mx-auto px-4 py-24'>
       <div className='w-full flex flex-col items-center '>
@@ -14,6 +19,166 @@ const Archive = () => {
           techs={['Reactjs', 'Tailwindcss', 'Nextjs']}
           link=''
         />
+        <ArchiveCard 
+          title='Project Name'
+          des='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+          techs={['Reactjs', 'Tailwindcss', 'Nextjs']}
+          link=''
+        />
+        <ArchiveCard 
+          title='Project Name'
+          des='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+          techs={['Reactjs', 'Tailwindcss', 'Nextjs']}
+          link=''
+        />
+        <ArchiveCard 
+          title='Project Name'
+          des='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+          techs={['Reactjs', 'Tailwindcss', 'Nextjs']}
+          link=''
+        />
+        <ArchiveCard 
+          title='Project Name'
+          des='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+          techs={['Reactjs', 'Tailwindcss', 'Nextjs']}
+          link=''
+        />
+        <ArchiveCard 
+          title='Project Name'
+          des='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+          techs={['Reactjs', 'Tailwindcss', 'Nextjs']}
+          link=''
+        />
+
+        { showMore && (
+          <>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{ delay: 0.1 }}
+            >
+              <ArchiveCard 
+                title='Project Name'
+                des='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+                techs={['Reactjs', 'Tailwindcss', 'Nextjs']}
+                link=''
+              />
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{ delay: 0.2 }}
+            >
+              <ArchiveCard 
+                title='Project Name'
+                des='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+                techs={['Reactjs', 'Tailwindcss', 'Nextjs']}
+                link=''
+              />
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{ delay: 0.3 }}
+            >
+              <ArchiveCard 
+                title='Project Name'
+                des='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+                techs={['Reactjs', 'Tailwindcss', 'Nextjs']}
+                link=''
+              />
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{ delay: 0.1 }}
+            >
+              <ArchiveCard 
+                title='Project Name'
+                des='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+                techs={['Reactjs', 'Tailwindcss', 'Nextjs']}
+                link=''
+              />
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{ delay: 0.2 }}
+            >
+              <ArchiveCard 
+                title='Project Name'
+                des='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+                techs={['Reactjs', 'Tailwindcss', 'Nextjs']}
+                link=''
+              />
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{ delay: 0.3 }}
+            >
+              <ArchiveCard 
+                title='Project Name'
+                des='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+                techs={['Reactjs', 'Tailwindcss', 'Nextjs']}
+                link=''
+              />
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{ delay: 0.1 }}
+            >
+              <ArchiveCard 
+                title='Project Name'
+                des='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+                techs={['Reactjs', 'Tailwindcss', 'Nextjs']}
+                link=''
+              />
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{ delay: 0.2 }}
+            >
+              <ArchiveCard 
+                title='Project Name'
+                des='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+                techs={['Reactjs', 'Tailwindcss', 'Nextjs']}
+                link=''
+              />
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{ delay: 0.3 }}
+            >
+              <ArchiveCard 
+                title='Project Name'
+                des='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+                techs={['Reactjs', 'Tailwindcss', 'Nextjs']}
+                link=''
+              />
+            </motion.div>
+          </>
+        )}
+      </div>
+      <div className='mt-12 flex items-center justify-center'>
+        <button
+          onClick={() => setShowMore(!showMore)}
+          className='w-36 h-12 rounded-md text-textGreen text-[13px] border border-textGreen hover:bg-hoverColor duration-300'
+        >
+          { showMore ? "Show Less" : "Show More" }
+        </button>
       </div>
     </div>
   )
